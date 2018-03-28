e9a50a67b94306ffe3c75694a1f06c6021e5f106
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-03-28 13:47:59 -0400

    _

--- JavaScript/random_chance.js
+++ JavaScript/random_chance.js
@@ -5,22 +5,33 @@ function games(quarters) {
     var leftover = quarters;
     for(var i = 0; i < quarters; ++i){
       leftover--;
-    var random = Math.floor((Math.random() * 100 ) + 1);
+      var random = Math.floor((Math.random() * 100 ) + 1);
       var coins = Math.floor((Math.random() * 100 ) + 1);
       if(coins < 50){
         coins = coins + 50;
-        }
+      }
       if(random === win){
         console.log("you won", coins, leftover);
-          return;
-        }else{
+        return;
+      }else{
           if(leftover != 0){
             console.log("sorry try again");
-            }else{
-          console.log("no more coins left");
-            }
+          }else{
+            console.log("no more coins left");
           }
+      }
     }
      
   }
-  games(5);
+  // games(35);
+  var arr = [1,2,3];
+  function reverse(arr) {
+    console.log(Math.floor(arr.length/2))
+    for(var i = 0; i < arr.length/2; i++){
+      var temp = arr[i];
+      arr[i]= arr[arr.length-i-1];
+      arr[arr.length - i - 1] = temp;
+    }
+    console.log(arr);
+  }
+  reverse(arr);
