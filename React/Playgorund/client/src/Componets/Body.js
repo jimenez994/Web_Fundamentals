820aa271a2947752189d91fc09031334dc375646
Jonathan Jimenez <j.jimenez.994@hotmail.com>
2018-09-15 15:43:06 -0400

    _

---
+++ React/Playgorund/client/src/Componets/Body.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Body extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Body ..... </h4>
+      </div>
+    );
+  }
+}
+
+export default Body;
---
+++ React/Playgorund/client/src/Views/Pr1.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Header from '../Componets/Header'
+import Body from '../Componets/Body'
+import Footer from '../Componets/Footer'
+
+class Pr1 extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Body/>
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+
+export default Pr1;
